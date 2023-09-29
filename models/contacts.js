c0dd9ccd29db197734950c8a924e5acd7755118d
Bohdan99I <bohdan.voiyov@gmail.com>
2023-09-29 21:07:12 +0300

    Fix:+будь яке проперті

--- models/contacts.js
+++ models/contacts.js
@@ -41,14 +41,18 @@ const addContact = async (body) => {
 const updateContact = async (contactId, body) => {
   const contacts = await listContacts();
   const index = contacts.findIndex((contact) => contact.id === contactId);
+
   if (index === -1) {
     return null;
   }
-  
-  contacts[index] = { id: contactId, ...body };
+
+  const updatedContact = { ...contacts[index], ...body };
+  contacts[index] = updatedContact;
+
   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-  return contacts[index];
+  return updatedContact;
 };
+
 
 module.exports = {
   listContacts,
