3d0b1f09c6ae63b112db6b44c844d7dc21597b3d
Bohdan99I <bohdan.voiyov@gmail.com>
2023-10-05 19:46:38 +0300

    aaa

--- server.js
+++ server.js
@@ -9,11 +9,10 @@ mongoose.set("strictQuery", true);
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(3000, () => {
-      console.log("Database connection successful");
-    });
+    app.listen(3000);
+    console.log("Database connection successful");
   })
   .catch((error) => {
     console.log(error.message);
     process.exit(1);
-  });
+  }); 
