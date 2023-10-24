48bd7042ef313bdf7f6e6072d6ed0024c47d0199
Bohdan99I <bohdan.voiyov@gmail.com>
2023-10-24 21:37:16 +0300

    hw-06

--- models/user.js
+++ models/user.js
@@ -27,10 +27,18 @@ const userSchema = new Schema(
       type: String,
       default: "",
     },
+    verify: {
+      type: Boolean,
+      default: false,
+    },
+    verificationToken: {
+      type: String,
+      required: [true, "Verify token is required"],
+    },
     avatarURL: {
       type: String,
       require: true,
-    }
+    },
   },
   { versionKey: false, timestamps: true }
 );
